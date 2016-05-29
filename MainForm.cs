2c1afa199bef81b7f70c3efb3946e42156e7536a
James <collijk@uw.edu>
2016-05-29 13:04:12 -0700

    Move analysisFileWriter to services

--- MainForm.cs
+++ MainForm.cs
@@ -8,6 +8,7 @@ using System.Windows.Forms;
 
 using Janus.ManagedMIDI;
 using Janus.Misc;
+using ChordQuality.Services;
 
 namespace ChordQuality
 {
@@ -1068,7 +1069,7 @@ namespace ChordQuality
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel2.Location = new System.Drawing.Point(0, 200);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1049, 287);
+            this.panel2.Size = new System.Drawing.Size(1049, 245);
             this.panel2.TabIndex = 46;
             this.panel2.Resize += new System.EventHandler(this.Panel2Resize);
             // 
@@ -1269,7 +1270,7 @@ namespace ChordQuality
             // MainForm
             // 
             this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(1049, 487);
+            this.ClientSize = new System.Drawing.Size(1049, 445);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Menu = this.mainMenu1;
@@ -2468,94 +2469,4 @@ namespace ChordQuality
 }
 
 
-public class AnalysisFileWriter
-{
-	public AnalysisFileWriter(string filename)
-	{
-		sw = File.CreateText(filename);
-	}
-	public void WriteTracks(MidiFile f, int t)
-	{
-		sw.WriteLine("<<< SOURCE FILE >>>");
-		sw.WriteLine(f.name);
-		sw.WriteLine();
-		sw.WriteLine("<<< TRANSPOSE >>>");
-		sw.WriteLine(t);
-		sw.WriteLine();
-		sw.WriteLine("<<< TRACKS >>>");
-		for (int i = 0; i < f.tracks.Length; i++)
-		{
-			sw.Write("[");
-			if (f.tracks[i].enabled) sw.Write("X");
-			else sw.Write(" ");
-			sw.WriteLine("] #" + (i + 1) + ": " + f.tracks[i].name);
-		}
-		sw.WriteLine();
-	}
-	public void WriteTunings(TuningScheme[] tun)
-	{
-		sw.WriteLine("<<< TUNINGS >>>");
-		for (int i = 0; i < tun.Length; i++)
-		{
-			if (tun[i].enabled)
-				sw.WriteLine(tun[i].ToString());
-		}
-		sw.WriteLine();
-	}
-	public void WriteTuningTranspose(int t)
-	{
-		sw.WriteLine("<<< TUNING TRANSPOSE >>>");
-		sw.WriteLine(t);
-		sw.WriteLine();
-	}
-	public void WriteWeights(QualityWeights w, DomainUpDown tud)
-	{
-		sw.WriteLine("<<< QUALITY WEIGHTS >>>");
-		sw.WriteLine("INTERVALS:");
-		sw.WriteLine("6M:\t" + w.M6);
-		sw.WriteLine("6m:\t" + w.m6);
-		sw.WriteLine("5:\t" + w.fifth);
-		sw.WriteLine("4:\t" + w.fourth);
-		sw.WriteLine("3M:\t" + w.M3);
-		sw.WriteLine("3m:\t" + w.m3);
-		sw.WriteLine("CHORDS:");
-		sw.WriteLine("5:\t" + w.Ch5);
-		sw.WriteLine("3M:\t" + w.Ch3);
-		sw.WriteLine();
-		sw.WriteLine("<<< PENALTIES >>>");
-		sw.WriteLine("Additional Notes:\t" + w.Add);
-		sw.WriteLine("Short Notes:\t" + w.Short);
-		sw.WriteLine("Threshold for Short Notes:\t" + tud.Text);
-		sw.WriteLine();
-	}
-	public void WriteChords(ArrayList chords, MidiFile f, TuningScheme[] t, QualityWeights w)
-	{
-		sw.WriteLine();
-		sw.WriteLine("<<< CHORDS & INTERVALS >>>");
-		sw.WriteLine();
-		sw.Write("POS\tNAME\tDUR");
-		for (int i = 0; i < t.Length; i++)
-			if (t[i].enabled) sw.Write("\t" + t[i].Name);
-		sw.WriteLine();
-		foreach (TimeInfo c in chords)
-		{
-			if (!w.enabled(c)) continue;
-			sw.Write(Math.Round(c.Time / 4.0 / f.timing, 2) + "\t" + c.Name + "\t" + Math.Round(c.Duration / 4.0 / f.timing, 2));
-			for (int i = 0; i < t.Length; i++)
-				if (t[i].enabled) sw.Write("\t" + t[i].Quality(c, w));
-			sw.WriteLine();
-		}
-		sw.Write("POS\tNAME\tDUR");
-		for (int i = 0; i < t.Length; i++)
-			if (t[i].enabled) sw.Write("\t" + t[i].Name);
-		sw.WriteLine();
-		sw.Write("AVERAGE:\t\t");
-		for (int i = 0; i < t.Length; i++)
-			if (t[i].enabled) sw.Write("\t" + Math.Round(t[i].AvgQuality(chords, w), 1));
-	}
-	public void Close()
-	{
-		sw.Close();
-	}
-	protected StreamWriter sw;
-}
+
---
+++ Services/AnalysisFileWriter.cs
@@ -0,0 +1,111 @@
+﻿using Janus.ManagedMIDI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChordQuality.Services
+{
+    public class AnalysisFileWriter
+    {
+        public AnalysisFileWriter(string filename)
+        {
+            sw = File.CreateText(filename);
+        }
+        public void WriteTracks(MidiFile f, int t)
+        {
+            sw.WriteLine("<<< SOURCE FILE >>>");
+            sw.WriteLine(f.name);
+            sw.WriteLine();
+            sw.WriteLine("<<< TRANSPOSE >>>");
+            sw.WriteLine(t);
+            sw.WriteLine();
+            sw.WriteLine("<<< TRACKS >>>");
+            for(int i = 0; i < f.tracks.Length; i++)
+            {
+                sw.Write("[");
+                if(f.tracks[i].enabled)
+                    sw.Write("X");
+                else
+                    sw.Write(" ");
+                sw.WriteLine("] #" + (i + 1) + ": " + f.tracks[i].name);
+            }
+            sw.WriteLine();
+        }
+        public void WriteTunings(TuningScheme[] tun)
+        {
+            sw.WriteLine("<<< TUNINGS >>>");
+            for(int i = 0; i < tun.Length; i++)
+            {
+                if(tun[i].enabled)
+                    sw.WriteLine(tun[i].ToString());
+            }
+            sw.WriteLine();
+        }
+        public void WriteTuningTranspose(int t)
+        {
+            sw.WriteLine("<<< TUNING TRANSPOSE >>>");
+            sw.WriteLine(t);
+            sw.WriteLine();
+        }
+        public void WriteWeights(QualityWeights w, DomainUpDown tud)
+        {
+            sw.WriteLine("<<< QUALITY WEIGHTS >>>");
+            sw.WriteLine("INTERVALS:");
+            sw.WriteLine("6M:\t" + w.M6);
+            sw.WriteLine("6m:\t" + w.m6);
+            sw.WriteLine("5:\t" + w.fifth);
+            sw.WriteLine("4:\t" + w.fourth);
+            sw.WriteLine("3M:\t" + w.M3);
+            sw.WriteLine("3m:\t" + w.m3);
+            sw.WriteLine("CHORDS:");
+            sw.WriteLine("5:\t" + w.Ch5);
+            sw.WriteLine("3M:\t" + w.Ch3);
+            sw.WriteLine();
+            sw.WriteLine("<<< PENALTIES >>>");
+            sw.WriteLine("Additional Notes:\t" + w.Add);
+            sw.WriteLine("Short Notes:\t" + w.Short);
+            sw.WriteLine("Threshold for Short Notes:\t" + tud.Text);
+            sw.WriteLine();
+        }
+        public void WriteChords(ArrayList chords, MidiFile f, TuningScheme[] t, QualityWeights w)
+        {
+            sw.WriteLine();
+            sw.WriteLine("<<< CHORDS & INTERVALS >>>");
+            sw.WriteLine();
+            sw.Write("POS\tNAME\tDUR");
+            for(int i = 0; i < t.Length; i++)
+                if(t[i].enabled)
+                    sw.Write("\t" + t[i].Name);
+            sw.WriteLine();
+            foreach(TimeInfo c in chords)
+            {
+                if(!w.enabled(c))
+                    continue;
+                sw.Write(Math.Round(c.Time / 4.0 / f.timing, 2) + "\t" + c.Name + "\t" + Math.Round(c.Duration / 4.0 / f.timing, 2));
+                for(int i = 0; i < t.Length; i++)
+                    if(t[i].enabled)
+                        sw.Write("\t" + t[i].Quality(c, w));
+                sw.WriteLine();
+            }
+            sw.Write("POS\tNAME\tDUR");
+            for(int i = 0; i < t.Length; i++)
+                if(t[i].enabled)
+                    sw.Write("\t" + t[i].Name);
+            sw.WriteLine();
+            sw.Write("AVERAGE:\t\t");
+            for(int i = 0; i < t.Length; i++)
+                if(t[i].enabled)
+                    sw.Write("\t" + Math.Round(t[i].AvgQuality(chords, w), 1));
+        }
+        public void Close()
+        {
+            sw.Close();
+        }
+        protected StreamWriter sw;
+    }
+}
