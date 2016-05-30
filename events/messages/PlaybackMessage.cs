269aa4aaed07f03fc8208b6393c72f6b833879f2
James <collijk@uw.edu>
2016-05-30 00:47:50 -0700

    Added singleton implementation to EventAggregator and created PlaybackMessage and derived classes

---
+++ events/messages/PlaybackMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChordQuality.events.messages
+{
+    class PlaybackMessage
+    {
+    }
+}
